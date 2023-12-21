deb863ad7ac79a1fe2693ceb0c6781af80ca4396
Gorbkos <gniewkoscielak@gmail.com>
2023-12-21 14:28:50 +0100

    initial commit

---
+++ src/src/z2/Drukarka.java
@@ -0,0 +1,43 @@
+package src.z2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Drukarka {
+    public void start(String inputFile, String outputFile) {
+        try {
+            File inFile = new File(inputFile);
+            Scanner fileScanner = new Scanner(inFile);
+            PrintWriter fileWriter = new PrintWriter(outputFile);
+
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (line.equals("drukuj")) {
+                    printResults(fileWriter);
+                } else {
+                    processNumericValue(line);
+                }
+            }
+
+            fileScanner.close();
+            fileWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + inputFile);
+        }
+    }
+
+    private void processNumericValue(String line) {
+        try {
+            int value = Integer.parseInt(line.trim());
+            System.out.println(value);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid numeric value: " + line);
+        }
+    }
+
+    private void printResults(PrintWriter writer) {
+        System.out.println("Printing results...");
+    }
+}
---
+++ src/src/z2/Main.java
@@ -0,0 +1,8 @@
+package src.z2;
+
+public class Main {
+    public static void main(String[] args) {
+        Drukarka drukarka = new Drukarka();
+        drukarka.start("src/z2/dane1.txt", "src/z2/wynik1.txt");
+    }
+}
